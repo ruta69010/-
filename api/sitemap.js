b36c97e15419b7363c648a8aa5af65d6585d487c
ruta69010 <ruta69010@gmail.com>
2026-06-16 17:58:19 +0900

    Update sitemap.js

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,6 +1,7 @@
 export default function handler(req, res) {
-  res.setHeader("Content-Type", "application/xml");
-  res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
+  res.setHeader("Content-Type", "text/xml; charset=utf-8");
+  res.setHeader("Cache-Control", "public, max-age=3600");
+  res.status(200).end(`<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://umazen-amber.vercel.app/</loc>
